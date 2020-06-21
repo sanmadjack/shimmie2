8c663330986e32eb828fcc10e12bf8c452d7c33f
Matthew Barbour <matthew@darkholme.net>
2020-06-21 14:27:01 -0500

    Extended autocomplete plugin to work for just about all tag entry fields

--- ext/autocomplete/main.php
+++ ext/autocomplete/main.php
@@ -1,5 +1,15 @@
 <?php declare(strict_types=1);
 
+
+class AutoCompleteConfig
+{
+    public const SEARCH_LIMIT = "autocomplete_search_limit";
+    public const SEARCH_CATEGORIES = "autocomplete_search_categories";
+    public const NAVIGATION = "autocomplete_navigation";
+    public const TAGGING = "autocomplete_tagging";
+}
+
+
 class AutoComplete extends Extension
 {
     /** @var AutoCompleteTheme */
@@ -9,6 +19,15 @@ class AutoComplete extends Extension
     {
         return 30;
     } // before Home
+
+    public function onInitExt(InitExtEvent $event)
+    {
+        global $config;
+        $config->set_default_bool(AutoCompleteConfig::NAVIGATION, true);
+        $config->set_default_bool(AutoCompleteConfig::TAGGING, false);
+        $config->set_default_int(AutoCompleteConfig::SEARCH_LIMIT, 20);
+        $config->set_default_bool(AutoCompleteConfig::SEARCH_CATEGORIES, false);
+    }
 
     public function onPageRequest(PageRequestEvent $event)
     {
@@ -75,5 +94,10 @@ class AutoComplete extends Extension
         }
 
         return $res;
+    }
+
+    public function onSetupBuilding(SetupBuildingEvent $event)
+    {
+        $this->theme->display_admin_block($event);
     }
 }
