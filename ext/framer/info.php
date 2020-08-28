13526e0969b4896a5a4b41cdf80603a8bbbfbca3
Matthew Barbour <matthew@darkholme.net>
2020-08-28 09:12:29 -0500

    Framer extension

---
+++ ext/framer/info.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+class FramerInfo extends ExtensionInfo
+{
+    public const KEY = "framer";
+
+    public $key = self::KEY;
+    public $name = "Framer";
+    public $url = self::SHIMMIE_URL;
+    public $authors = "Matthew Barbour";
+    public $license = self::LICENSE_WTFPL;
+}
